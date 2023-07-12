985d1b8db8efd7823b1c70dfe6f48f3759b36363
Abiola Zeenat Abdulsalam <81884414+Abiola-Zeenat@users.noreply.github.com>
2023-07-12 13:07:40 +0100

    collaboration setup

--- BookstoreAPI/Program.cs
+++ BookstoreAPI/Program.cs
@@ -1,4 +1,23 @@
+using AutoMapper;
+using BookStore__Management_system.Data;
+using BookStore__Management_system.Helpers;
+using BookStore__Management_system.Repository;
+using Cqrs.Hosts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BookStoreContext>(options => options.UseSqlServer(
+builder.Configuration.GetConnectionString("DefaultConnection")
+));
+
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddTransient<IBookRepository, BookRepository>();
+builder.Services.AddTransient<ISaleRepository, SaleRepository>();
+builder.Services.AddAutoMapper(typeof(ApplicationMapper));
+
 
 // Add services to the container.
 
